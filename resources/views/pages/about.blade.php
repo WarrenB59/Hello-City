4d7cb272a5a1b0b86d4f1678528395e1ee3a5ece
WarrenB59 <bwarren90@hotmail.fr>
2022-01-24 00:11:21 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>A Propos | Hello City</title>
-    </head>
-    <body>
+@section('content')
         <p>Codé avec &hearts; par WARREN</p>
 
         <p><a href="/">Revenir vers la page d'accueil</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Hello City</title>
-    </head>
-    <body>
+@section('content')
         <h1>Bonjour de France !</h1>
 
         <p>Il est actuellement {{date('H:i:s')}}</p>
 
-        <footer>
-            <p>
-                &copy; Copyright {{date('Y')}}
-                &middot; <a href="about-us">A Propos</a>
-            </p>
-        </footer>
-    </body>
-</html>
+@endsection
