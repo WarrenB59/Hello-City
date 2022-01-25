7b43af2796b469afabde8e3af1ede90c409a950b
WarrenB59 <bwarren90@hotmail.fr>
2022-01-25 23:00:11 +0100

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'A propos | ' . config('app.name'))
 
 @section('content')
+        <img src="images/dev_icon.png" alt="Developer Icon">
         <p>Codé avec &hearts; par WARREN</p>
 
         <p><a href="{{ route('home')}}">Revenir vers la page d'accueil</a></p>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,7 @@
 @extends('app')
 
 @section('content')
+        <img src="images/Flag_of_France.svg" alt="France Flag">
         <h1>Bonjour de France !</h1>
 
         <p>Il est actuellement {{date('H:i:s')}}</p>
